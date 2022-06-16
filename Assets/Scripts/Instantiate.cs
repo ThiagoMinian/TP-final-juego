62cf58f53e2efd5a62be06ab61a5613a5972d1d8
ThiagoMinian <101407767+ThiagoMinian@users.noreply.github.com>
2022-06-16 10:31:53 -0300

    Termine juego

---
+++ Assets/Scripts/Instantiate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiate : MonoBehaviour
+{
+    public GameObject cubePrefab, clone;
+    public float i = 6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            while (i > 0)
+            {
+                i--;
+                clone = Instantiate(cubePrefab, transform.position, Quaternion.identity);
+                Destroy(clone, 3);
+            }
+
+        }
+    }
+}
